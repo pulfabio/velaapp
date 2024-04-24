0c49fb7c4c34ac7a8c47b1a2dec3822ad5daa792
fabs <fabio.pulito@besquare.it>
2024-04-24 17:50:31 +0700

    added all the functionalities for assets and locations in home page mvvm, both manual input and ZXing reader, tested for manual input, not for bar code yet

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace velaapp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace velaapp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
